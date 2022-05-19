6e6c427f71fe20f0c41d7a283823fe328a3f72f2
George Kouros <george.kouros.ece@gmail.com>
2022-05-19 15:12:51 +0200

    Update job file

--- condor/run_nerf_job.sh
+++ condor/run_nerf_job.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
-echo 'NeRF job started'
+echo 'NeRD job started'
+
 NAME=$1
 EXP=$2
-source /users/visics/gkouros/.bashrc
+
 export PATH="/usr/local/cuda-11/bin:/usr/local/cuda/bin:$PATH"
 export LD_LIBRARY_PATH="/usr/local/cuda-11/lib64:/usr/local/cuda/lib64:$LD_LIBRARY_PATH"
+
+source ~/miniconda3/etc/profile.d/conda.sh
 conda activate nerd
+
 DIR=/users/visics/gkouros/projects/nerf-repos/NeRD-Neural-Reflectance-Decomposition/
 cd $DIR
 
 python3 train_nerd.py --datadir /esat/topaz/gkouros/datasets/nerf/$NAME --basedir logs/$NAME --expname $EXP --gpu $CUDA_VISIBLE_DEVICES --config configs/nerd/real_world.txt --spherify
 
 conda deactivate
-echo 'NeRF job terminated'
+echo 'NeRD job terminated'
