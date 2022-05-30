b8e30c6ea814384cae2079a529241bf3938ce340
George Kouros <george.kouros.ece@gmail.com>
2022-05-30 12:20:17 +0200

    Update job requirements

--- condor/run_nerf_job.sh
+++ condor/run_nerf_job.sh
@@ -13,7 +13,7 @@ conda activate nerd
 DIR=/users/visics/gkouros/projects/nerf-repos/NeRD-Neural-Reflectance-Decomposition/
 cd $DIR
 
-python3 train_nerd.py --datadir /esat/topaz/gkouros/datasets/nerf/$NAME --basedir logs/$NAME --expname $EXP --gpu $CUDA_VISIBLE_DEVICES --config configs/nerd/real_world.txt --spherify
+python3 train_nerd.py --datadir /esat/topaz/gkouros/datasets/nerf/$NAME --basedir logs/$NAME --expname exp-$EXP --gpu $CUDA_VISIBLE_DEVICES --config configs/nerd/real_world.txt --spherify
 
 conda deactivate
 echo 'NeRD job terminated'
